06c3fca0e619ebcfa6730d95e34a995fee76fc6e
ayvc0420 <ayvc0420@gmail.com>
2022-01-05 14:49:15 +0800

    對IE瀏覽器進行新的顯示畫面

---
+++ js/isIE.js
@@ -0,0 +1,7 @@
+var iE = /*@cc_on!@*/false || !!document.documentMode;
+document.documentElement.innerHTML = '<div style="text-align:center;margin-top:50px;"><h1>本網站不支援IE瀏覽器，請切換<a href="https://www.google.com/intl/zh-TW/chrome/" style="color:rgb(0,0,255)">Chrome</a>或其他瀏覽器進行瀏覽。</h1></div>'
+if(iE){
+    setInterval(function(){
+        document.documentElement.innerHTML = '<div style="text-align:center;margin-top:50px;"><h1>本網站不支援IE瀏覽器，請切換<a href="https://www.google.com/intl/zh-TW/chrome/" style="color:rgb(0,0,255)">Chrome</a>或其他瀏覽器進行瀏覽。</h1></div>'
+    }, 2000);
+}
