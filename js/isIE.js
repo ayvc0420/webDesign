578b094e4581b01fa4e2a8c454a71e5b1fb41e87
ayvc0420 <ayvc0420@gmail.com>
2022-01-05 14:52:47 +0800

    IIFE

--- js/isIE.js
+++ js/isIE.js
@@ -1,7 +1,9 @@
-var iE = /*@cc_on!@*/false || !!document.documentMode;
-if(iE){
-    document.documentElement.innerHTML = '<div style="text-align:center;margin-top:50px;"><h1>本網站不支援IE瀏覽器，請切換<a href="https://www.google.com/intl/zh-TW/chrome/" style="color:rgb(0,0,255)">Chrome</a>或其他瀏覽器進行瀏覽。</h1></div>'
-    setInterval(function(){
+;(function(){
+    var iE = /*@cc_on!@*/false || !!document.documentMode;
+    if(iE){
         document.documentElement.innerHTML = '<div style="text-align:center;margin-top:50px;"><h1>本網站不支援IE瀏覽器，請切換<a href="https://www.google.com/intl/zh-TW/chrome/" style="color:rgb(0,0,255)">Chrome</a>或其他瀏覽器進行瀏覽。</h1></div>'
-    }, 2000);
-}
+        setInterval(function(){
+            document.documentElement.innerHTML = '<div style="text-align:center;margin-top:50px;"><h1>本網站不支援IE瀏覽器，請切換<a href="https://www.google.com/intl/zh-TW/chrome/" style="color:rgb(0,0,255)">Chrome</a>或其他瀏覽器進行瀏覽。</h1></div>'
+        }, 2000);
+    }
+})()
